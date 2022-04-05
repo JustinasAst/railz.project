b0f9d549abee482672089d468fd207b0a71d3fe1
Justinas Astrauskas <justinas.astrauskas@nfq001426.foreman.nfq.lt>
2022-04-05 17:17:47 +0300

    add data to pages

--- pages/railz-connect.tsx
+++ pages/railz-connect.tsx
@@ -4,7 +4,71 @@ const RailzConnect = () => {
   return (
     <div className="main-railz-container">
       <div className="page-container">
-        <h1 className="railz">Railz Connect</h1>
+        <div className="main-article-section">
+          <div className="article-section">
+            <div className="get-full-access-section">
+              <h1>Get started with Railz Connect™</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A animi
+                fugiat autem corporis labore, ab tenetur aliquid!
+              </p>
+              <button className="article-btn">Get started</button>
+            </div>
+
+            <div className="logo-component">
+              <img
+                src="https://railz.ai/_next/image?url=https%3A%2F%2Fa.storyblok.com%2Ff%2F124388%2Fx%2F671efaf61f%2Frailz-connect-product-page-hero-584x520.svg&w=1920&q=80"
+                alt="railz-api"
+              />
+            </div>
+          </div>
+
+          <div className="article-about">
+            <h2 className="article-about-title">
+              Why you should use the Railz Connect™ for your financial service
+            </h2>
+            <div className="article-about-benefits-container">
+              <div className="article-about-box connect">
+                <img
+                  src="https://railz.ai/_next/image?url=https%3A%2F%2Fa.storyblok.com%2Ff%2F124388%2Fx%2F2e41094b78%2Fconnect-64x64.svg&w=1920&q=80"
+                  alt="railz connect"
+                />
+                <h3>White Labelling</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
+                  unde, veritatis assumenda quos rerum ab alias aliquam est
+                  nesciunt vitae.
+                </p>
+              </div>
+
+              <div className="article-about-box sandbox-mode">
+                <img
+                  src="https://railz.ai/_next/image?url=https%3A%2F%2Fa.storyblok.com%2Ff%2F124388%2Fx%2F631a8591aa%2Fsandbox-64x64.svg&w=1920&q=80"
+                  alt="railz sandbox mode"
+                />
+                <h3>Easy Access</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                  aperiam excepturi vero amet iste quis quod eos rerum, hic
+                  illum.
+                </p>
+              </div>
+
+              <div className="article-about-box data-endpoints">
+                <img
+                  src="https://railz.ai/_next/image?url=https%3A%2F%2Fa.storyblok.com%2Ff%2F124388%2Fx%2F63509d8954%2F3-endpoints-64x64.svg&w=1920&q=80"
+                  alt="railz data endpoints"
+                />
+                <h3>Real-Time</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorum quibusdam quae laudantium! Laboriosam suscipit quae
+                  placeat consequatur dolore necessitatibus! Dolores!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
